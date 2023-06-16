f1b9ab159b8c5ac3fc990bf7b4af67c079ff91da
sourcecodestudiouk <connor.banks@sourcecodestudio.co.uk>
2023-06-16 09:55:53 +0100

    Change navigation menu warning message

--- wp-content/themes/liteSource/functions/admin-notices.php
+++ wp-content/themes/liteSource/functions/admin-notices.php
@@ -4,7 +4,7 @@ function general_admin_notice(){
     global $pagenow;
     if ( $pagenow == 'nav-menus.php' ) {
          echo '<div class="notice notice-warning is-dismissible">
-             <p>Navigation Menus can only contain 5 top level items, the rest will automatically go into a dropdown menu.</p>
+             <p>The Header Navigation Menu can only contain 5 top level items, the rest will automatically go into a dropdown menu.</p>
          </div>';
     }
 }
